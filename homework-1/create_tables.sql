f152cbf60adf19ad045c263c3791df8f4f18a6c1
Skulsik_coder <skulsik1@gmail.com>
2023-04-07 11:42:49 +0700

    Copy project, after fatal error...

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,25 @@
 -- SQL-команды для создания таблиц
+CREATE DATABASE north;
+CREATE TABLE employees_data
+(
+    employee_id serial PRIMARY KEY,
+	first_name varchar(15) NOT NULL,
+	last_name varchar(15) NOT NULL,
+    title varchar(100) NOT NULL,
+	birth_date date,
+	notes text
+);
+CREATE TABLE customers_data
+(
+    customer_id varchar(5) PRIMARY KEY,
+	company_name varchar(100) NOT NULL,
+	contact_name varchar(50) NOT NULL
+);
+CREATE TABLE orders_data
+(
+	order_id serial PRIMARY KEY,
+    customer_id varchar(5) REFERENCES customers_data(customer_id) NOT NULL,
+	employee_id int NOT NULL,
+	order_date date NOT NULL,
+	ship_city varchar(30)
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,16 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+
+# connect to BD
+with psycopg2.connect(host='localhost', database='', user='postgres', password='12345') as conn:
+    # greate cursor
+    with conn.cursor() as cur:
+        # execute query
+        cur.execute('INSERT INTO table VALUES (%s, %s)', (3, 'fdgf'))
+
+        cur.execute('SELECT * FROM table')
+        # get info
+        cur.fetchall()
+
+# close cursor and connect
+conn.close()
